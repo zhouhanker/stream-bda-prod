b2d5c7813594926481a403970edc77789c56867b
zhouhan <zhouhanker@gmail.com>
2025-11-06 22:10:49 +0800

    add v2 idx & ddl

--- script/doris216/ddl/lulu_report.sql
+++ script/doris216/ddl/lulu_report.sql
@@ -1,42 +1,7 @@
 create database bigdata_realtime_report_v3;
 
 
-drop table if exists bigdata_realtime_report_v3.report_lululemon_window_gmv_topN;
-create table if not exists bigdata_realtime_report_v3.report_lululemon_window_gmv_topN (
-    ds DATE NOT NULL COMMENT '分区字段，统计日期',
-    window_start STRING COMMENT '窗口开始时间',
-    window_end STRING COMMENT '窗口结束时间',
-    win_gmv DECIMAL(18,2) COMMENT '窗口累计gmv',
-    win_gmv_ids STRING COMMENT '窗口内商品id列表',
-    top5_product_ids STRING COMMENT '窗口内top5商品id'
-)
-engine = olap
-duplicate key (ds)
-comment 'lululemon 日内累计 gmv 窗口聚合结果'
-partition by range (ds) ()
-distributed by hash(ds) buckets 10
-properties (
-               "storage_format" = "v2",
-               "light_schema_change" = "true",
-               "replication_num" = "1",
-               "dynamic_partition.enable" = "true",
-               "dynamic_partition.time_unit" = "day",
-               "dynamic_partition.start" = "-30",
-               "dynamic_partition.end" = "30",
-               "dynamic_partition.prefix" = "p",
-               "dynamic_partition.buckets" = "10",
-               "dynamic_partition.create_history_partition" = "true",
-               "dynamic_partition.history_partition_num" = "30",
-               "dynamic_partition.enable_delete" = "true"
-);
-
-select *
-from bigdata_realtime_report_v3.report_lululemon_window_gmv_topN
-where ds = '2025-10-29';
-
-
-
--- log device tbl
+truncate table bigdata_realtime_report_v3.report_lululemon_day_log_device_info;
 CREATE TABLE IF NOT EXISTS bigdata_realtime_report_v3.report_lululemon_day_log_device_info (
     pt DATE NOT NULL COMMENT '分区日期',
     os VARCHAR(32) NOT NULL COMMENT '系统平台',
@@ -45,24 +10,26 @@ CREATE TABLE IF NOT EXISTS bigdata_realtime_report_v3.report_lululemon_day_log_d
     count BIGINT COMMENT '最新统计值',
     type VARCHAR(32) COMMENT '统计类型',
     update_time DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间'
-    )
-    UNIQUE KEY(pt, os, brand, platv)
-    PARTITION BY RANGE(pt)()
-    DISTRIBUTED BY HASH(pt, os, brand) BUCKETS 8
-    PROPERTIES (
-       "replication_allocation" = "tag.location.default: 1",
-       "enable_unique_key_merge_on_write" = "true",
-       "dynamic_partition.enable" = "true",
-       "dynamic_partition.time_unit" = "DAY",
-       "dynamic_partition.start" = "-30",
-       "dynamic_partition.end" = "30",
-       "dynamic_partition.prefix" = "p",
-       "dynamic_partition.buckets" = "8",
-       "dynamic_partition.create_history_partition" = "true"
+)
+UNIQUE KEY(pt, os, brand, platv)
+PARTITION BY RANGE(pt)()
+DISTRIBUTED BY HASH(pt, os, brand) BUCKETS 8
+PROPERTIES (
+    "replication_allocation" = "tag.location.default: 1",
+    "enable_unique_key_merge_on_write" = "true",
+    "dynamic_partition.enable" = "true",
+    "dynamic_partition.time_unit" = "DAY",
+    "dynamic_partition.start" = "-30",
+    "dynamic_partition.end" = "30",
+    "dynamic_partition.prefix" = "p",
+    "dynamic_partition.buckets" = "8",
+    "dynamic_partition.create_history_partition" = "true"
 );
 
--- region
-CREATE TABLE IF NOT EXISTS bigdata_realtime_report_v3.report_lululemon_day_region_info (
+
+truncate table bigdata_realtime_report_v3.report_lululemon_day_log_region_info;
+drop table if exists bigdata_realtime_report_v3.report_lululemon_day_log_region_info;
+CREATE TABLE IF NOT EXISTS bigdata_realtime_report_v3.report_lululemon_day_log_region_info (
     pt DATE NOT NULL COMMENT '分区日期',
     region VARCHAR(128) NOT NULL COMMENT '省市区组合（省|市|运营商）',
     province VARCHAR(64) COMMENT '省份',
@@ -76,16 +43,89 @@ CREATE TABLE IF NOT EXISTS bigdata_realtime_report_v3.report_lululemon_day_regio
     update_time DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间'
 )
 UNIQUE KEY(pt, region)
-PARTITION BY RANGE(pt) (PARTITION p_init VALUES LESS THAN ("2020-01-01"))
+PARTITION BY RANGE(pt) (
+    PARTITION p_init VALUES LESS THAN ("2020-01-01")    -- 模板分区，动态分区表必须存在
+)
 DISTRIBUTED BY HASH(pt, region) BUCKETS 8
 PROPERTIES (
-   "replication_allocation" = "tag.location.default: 1",
-   "enable_unique_key_merge_on_write" = "true",
-   "dynamic_partition.enable" = "true",
-   "dynamic_partition.time_unit" = "DAY",
-   "dynamic_partition.start" = "-30",
-   "dynamic_partition.end" = "30",
-   "dynamic_partition.prefix" = "p",
-   "dynamic_partition.buckets" = "8",
-   "dynamic_partition.create_history_partition" = "true"
+    "replication_allocation" = "tag.location.default: 1",
+    "enable_unique_key_merge_on_write" = "true",
+    "dynamic_partition.enable" = "true",
+    "dynamic_partition.time_unit" = "DAY",
+    "dynamic_partition.start" = "-30",
+    "dynamic_partition.end" = "30",
+    "dynamic_partition.prefix" = "p",
+    "dynamic_partition.buckets" = "8",
+    "dynamic_partition.create_history_partition" = "true"
 );
+
+select *
+from bigdata_realtime_report_v3.report_lululemon_day_log_region_info;
+
+
+truncate table bigdata_realtime_report_v3.report_lululemon_day_log_search_info;
+CREATE TABLE IF NOT EXISTS bigdata_realtime_report_v3.report_lululemon_day_log_search_info (
+    pt DATE NOT NULL COMMENT '分区日期',
+    search_item JSON COMMENT '搜索项明细',
+    emit_time DATETIME COMMENT 'flk数据产生时间',
+    update_time DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间'
+)
+DUPLICATE KEY(pt)
+PARTITION BY RANGE(pt) (
+    PARTITION p_init VALUES LESS THAN ("1970-01-01")   -- 模板分区
+)
+DISTRIBUTED BY HASH(pt) BUCKETS 8
+PROPERTIES (
+    "replication_allocation" = "tag.location.default: 1",
+    "dynamic_partition.enable" = "true",
+    "dynamic_partition.time_unit" = "DAY",
+    "dynamic_partition.start" = "-30",
+    "dynamic_partition.end" = "30",
+    "dynamic_partition.prefix" = "p",
+    "dynamic_partition.buckets" = "8",
+    "dynamic_partition.create_history_partition" = "true",
+    "storage_format" = "V2"
+);
+
+
+CREATE TABLE IF NOT EXISTS bigdata_realtime_report_v3.report_lululemon_day_log_page_info (
+    pt DATE NOT NULL COMMENT '分区日期',
+    page string COMMENT '搜索项明细',
+    pv bigint COMMENT 'flk数据产生时间',
+    user_ids json,
+    update_time DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间'
+)
+DUPLICATE KEY(pt)
+PARTITION BY RANGE(pt) (
+    PARTITION p_init VALUES LESS THAN ("1970-01-01")   -- 模板分区
+)
+DISTRIBUTED BY HASH(pt) BUCKETS 8
+PROPERTIES (
+    "replication_allocation" = "tag.location.default: 1",
+    "dynamic_partition.enable" = "true",
+    "dynamic_partition.time_unit" = "DAY",
+    "dynamic_partition.start" = "-30",
+    "dynamic_partition.end" = "30",
+    "dynamic_partition.prefix" = "p",
+    "dynamic_partition.buckets" = "8",
+    "dynamic_partition.create_history_partition" = "true",
+    "storage_format" = "V2"
+);
+
+
+truncate table bigdata_realtime_report_v3.report_lululemon_day_log_search_info;
+select *
+from bigdata_realtime_report_v3.report_lululemon_day_log_search_info;
+
+truncate table bigdata_realtime_report_v3.report_lululemon_day_log_device_info;
+select *
+from bigdata_realtime_report_v3.report_lululemon_day_log_device_info;
+
+truncate table bigdata_realtime_report_v3.report_lululemon_day_log_region_info;
+select *
+from bigdata_realtime_report_v3.report_lululemon_day_log_region_info;
+
+
+truncate table bigdata_realtime_report_v3.report_lululemon_day_log_page_info;
+select *
+from bigdata_realtime_report_v3.report_lululemon_day_log_page_info;
